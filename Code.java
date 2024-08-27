04fcc46d3e800a8e05311d3d37b03230596eab84
Nilesh <nilesh@Jagapathi-Varma-Datla.local>
2024-08-27 12:01:36 +0530

    Code Modified

--- Code.java
+++ Code.java
@@ -1,21 +1,71 @@
 import java.util.Scanner;
 
 public class Code {
+    static int num2, result;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int num1, num2, result;
+        int num1;
+        char operator;
 
         System.out.println("Enter first number:");
         num1 = scanner.nextInt();
 
-        System.out.println("Enter second number:");
-        num2 = scanner.nextInt();
+        System.out.println("Enter any operator(+,-,*,/,%):");
+        operator = scanner.next().charAt(0);
 
-        result = num1 + num2;
-
-        System.out.println("Result : " + result);
+        char[] allowedOperators = {'+', '-', '*', '%', '/'};
+        boolean isExist = contains(allowedOperators, operator);
         
 
+        if (isExist) {
+            System.out.println("Enter second number:");
+            num2 = scanner.nextInt();
+        }
+
+         
+
+        if (isExist) {
+            if (operator == '+') {
+                result = num1 + num2;
+      
+            } else if (operator == '-') {
+                result = num1 - num2;
+     
+            } else if (operator == '*') {
+                result = num1 * num2;
+     
+            } else if (operator == '/') {
+                result = num1 / num2;
+     
+            } else if (operator == '%') {
+                result = num1 % num2;
+     
+            } else {
+                System.out.println("Operator Not Found!");
+            }
+        }
+
+        if (isExist) {
+            System.out.println("Result : " + result );
+        } else {
+            System.out.println("Operator Not Found!");
+        }
+        
+
+    
+        
+
+    }
+
+    // Method to check if a char value exists in a char operators
+    private static boolean contains(char[] operators, char inputOperator) {
+        for (char element : operators) {
+            if (element == inputOperator) {
+                return true;
+            }
+        }
+        return false;
     }
 }
